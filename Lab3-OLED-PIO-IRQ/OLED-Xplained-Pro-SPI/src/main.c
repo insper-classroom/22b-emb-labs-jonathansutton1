02f8b2804931a5a64e8497ea8f1350318adeab8a
jonathansutton1 <62657975+jonathansutton1@users.noreply.github.com>
2022-08-31 17:36:46 -0300

    Lab3 conceito C

--- Lab3-OLED-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
+++ Lab3-OLED-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
@@ -4,42 +4,109 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
-int main (void)
-{
+//DEFINES
+#define LED_PIO      PIOC
+#define LED_PIO_ID   ID_PIOC
+#define LED_IDX      8
+#define LED_IDX_MASK (1 << LED_IDX)
+
+#define BUT1_PIO           PIOD
+#define BUT1_PIO_ID        ID_PIOD
+#define BUT1_PIO_IDX       28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+volatile char but_flag;
+//FUNCOES
+
+void but_callback(void){
+	if(!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)){  
+		but_flag = 1;
+	}else{
+		but_flag = 0;
+	}
+}
+
+// pisca led N vez no periodo T
+void pisca_led(int n, int t){
+	for (int i=0;i<n;i++){
+		pio_clear(LED_PIO, LED_IDX_MASK);
+		delay_ms(t);
+		pio_set(LED_PIO, LED_IDX_MASK);
+		delay_ms(t);
+	}
+}
+
+void init(void){
+	// Initialize the board clock
+	sysclk_init();
+
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	// Ativa o PIO na qual o LED foi conectado
+	// para que possamos controlar o LED.
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+	pio_set_output(LED_PIO, LED_IDX_MASK, 0, 0, 0);
+
+	
+	// Configura interrupção no pino referente ao botao e associa
+	// função de callback caso uma interrupção for gerada
+	// a função de callback é a: but1_callback()
+	pio_handler_set(BUT1_PIO,
+	BUT1_PIO_ID,
+	BUT1_PIO_IDX_MASK,
+	PIO_IT_EDGE,
+	but_callback);
+
+	// Ativa interrupção e limpa primeira IRQ gerada na ativacao
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT1_PIO);
+	
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
+}
+
+
+int main (void){
 	board_init();
 	sysclk_init();
 	delay_init();
+	init();
 
   // Init OLED
 	gfx_mono_ssd1306_init();
   
   
-	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
-  
-  
-
+	//gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+    //gfx_mono_draw_string("mundo", 50,16, &sysfont);
+	char str[128];
+	int freq = 200;
+	sprintf(str, "%d", freq); //
+	gfx_mono_draw_string(str, 0, 0, &sysfont);
+ 
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
-
-
-
-			// Escreve na tela um circulo e um texto
-			
-			for(int i=70;i<=120;i+=2){
-				
-				gfx_mono_draw_rect(i, 5, 2, 10, GFX_PIXEL_SET);
-				delay_ms(10);
-				
+		if(but_flag){
+			delay_ms(1000);
+			if(but_flag){    //usuario mantem o botao apertado
+				freq += 100;
+				sprintf(str, "%d", freq); //
+				gfx_mono_draw_string(str, 0, 0, &sysfont);
+				pisca_led(30,freq);
 			}
-			
-			for(int i=120;i>=70;i-=2){
-				
-				gfx_mono_draw_rect(i, 5, 2, 10, GFX_PIXEL_CLR);
-				delay_ms(10);
-				
-			}
-			
-			
+			else{
+				freq -= 100;
+				sprintf(str, "%d", freq); //
+				gfx_mono_draw_string(str, 0, 0, &sysfont);
+				pisca_led(30,freq);
+		}
+		
+		}
 	}
 }
