66c0350f2d1ac871f1d86eabf9d90d7aa81be1ff
jonathansutton1 <62657975+jonathansutton1@users.noreply.github.com>
2022-09-08 17:42:25 -0300

    Lab3 conceito B

--- Lab3-OLED-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
+++ Lab3-OLED-PIO-IRQ/OLED-Xplained-Pro-SPI/src/main.c
@@ -15,14 +15,42 @@
 #define BUT1_PIO_IDX       28
 #define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
 
-volatile char but_flag;
-//FUNCOES
+#define BUT2_PIO          PIOC
+#define BUT2_PIO_ID       ID_PIOC
+#define BUT2_PIO_IDX      31
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX) // esse já está pronto.
 
-void but_callback(void){
+#define BUT3_PIO          PIOA
+#define BUT3_PIO_ID       ID_PIOA
+#define BUT3_PIO_IDX      19
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX) // esse já está pronto.
+
+volatile char but1_flag;
+volatile char but2_flag;
+volatile char but3_flag;
+
+//FUNCOES
+void but1_callback(void){
 	if(!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)){  
-		but_flag = 1;
+		but1_flag = 1;
 	}else{
-		but_flag = 0;
+		but1_flag = 0;
+	}
+}
+
+void but2_callback(void){
+	if(!pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK)){
+		but2_flag = 1;
+	}else{
+		but2_flag = 0;
+	}
+}
+
+void but3_callback(void){
+	if(!pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK)){
+		but3_flag = 1;
+	}else{
+		but3_flag = 0;
 	}
 }
 
@@ -47,9 +75,18 @@ void init(void){
 	// para que possamos controlar o LED.
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
 	
 	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(BUT2_PIO,BUT2_PIO_IDX_MASK,PIO_DEFAULT);
+	pio_set_input(BUT3_PIO,BUT3_PIO_IDX_MASK,PIO_DEFAULT);
+
 	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+	pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+	pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+
+	
 	pio_set_output(LED_PIO, LED_IDX_MASK, 0, 0, 0);
 
 	
@@ -60,16 +97,38 @@ void init(void){
 	BUT1_PIO_ID,
 	BUT1_PIO_IDX_MASK,
 	PIO_IT_EDGE,
-	but_callback);
+	but1_callback);
+	
+	pio_handler_set(BUT2_PIO,
+	BUT2_PIO_ID,
+	BUT2_PIO_IDX_MASK,
+	PIO_IT_FALL_EDGE,
+	but2_callback);
+	
+	pio_handler_set(BUT3_PIO,
+	BUT3_PIO_ID,
+	BUT3_PIO_IDX_MASK,
+	PIO_IT_FALL_EDGE,
+	but3_callback);
 
 	// Ativa interrupção e limpa primeira IRQ gerada na ativacao
 	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
+	
 	pio_get_interrupt_status(BUT1_PIO);
+	pio_get_interrupt_status(BUT2_PIO);
+	pio_get_interrupt_status(BUT3_PIO);
+
 	
 	// Configura NVIC para receber interrupcoes do PIO do botao
 	// com prioridade 4 (quanto mais próximo de 0 maior)
 	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_EnableIRQ(BUT3_PIO_ID);
 	NVIC_SetPriority(BUT1_PIO_ID, 4); // Prioridade 4
+	NVIC_SetPriority(BUT2_PIO_ID, 4); // Prioridade 4
+	NVIC_SetPriority(BUT3_PIO_ID, 4); // Prioridade 4
 }
 
 
@@ -81,7 +140,7 @@ int main (void){
 
   // Init OLED
 	gfx_mono_ssd1306_init();
-  
+	int j = 30;
   
 	//gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
     //gfx_mono_draw_string("mundo", 50,16, &sysfont);
@@ -92,21 +151,42 @@ int main (void){
  
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
-		if(but_flag){
+		if(but1_flag){
 			delay_ms(1000);
-			if(but_flag){    //usuario mantem o botao apertado
+			if(but1_flag){    //usuario mantem o botao apertado
 				freq += 100;
 				sprintf(str, "%d", freq); //
 				gfx_mono_draw_string(str, 0, 0, &sysfont);
-				pisca_led(30,freq);
+				j = 30;
+				while(!but2_flag && j >=0){
+					pisca_led(15,freq);
+					j -= 1;
+				}
 			}
 			else{
 				freq -= 100;
 				sprintf(str, "%d", freq); //
 				gfx_mono_draw_string(str, 0, 0, &sysfont);
-				pisca_led(30,freq);
+				j = 30;
+				while(!but2_flag && j >=0){
+					pisca_led(15,freq);
+					j -= 1;
+				}
+			}
 		}
-		
+		//else if(but2_flag){
+			//pio_set(LED_PIO, LED_IDX_MASK);
+		//}
+		else if(but3_flag){
+			freq += 100;
+			sprintf(str, "%d", freq); //
+			gfx_mono_draw_string(str, 0, 0, &sysfont);
+			j = 30;
+			while(!but2_flag && j >=0){
+				pisca_led(15,freq);
+				j -= 1;
+			}
 		}
+		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI); //utilizar somente o modo sleep mode
 	}
 }
